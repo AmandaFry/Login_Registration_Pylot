597d9a53c2fecb822ae46b7361af491e2bdc2bf4
Amanda Fry <feamandaf@hotmail.com>
2016-06-28 15:36:44 -0700

    first push

---
+++ app/controllers/Travel.py
@@ -0,0 +1,48 @@
+from system.core.controller import *
+
+class Users(Controller):
+    def __init__(self, action):
+        super(Users, self).__init__(action)
+         # Note that we have to load the model before using it in the methods below
+        self.load_model('User')
+
+    # method to display registration page
+    def index(self):
+        #Loads home page
+        print 'I am in travel controller home page'
+        return self.load_view('index.html')
+
+
+    def create(self):
+        #register a new user - gather data posted to our create method and format it to pass it to the model
+        user_info = {
+            'f_name':request.form['f_name'],
+            'l_name':request.form['l_name'],
+            'email':request.form['email'],
+            'passw':request.form['passw'],
+            'conf_passw':request.form['conf_passw'],
+        }
+        # call create_user method from model and write some logic based on the returned value
+        # notice how we passed the user_info to our model method
+        create_status = self.models['User'].create_user(user_info)
+
+        if create_status['status'] == True:
+            # the user should have been created in the model
+            # we can set the newly-created users id and name to session
+            session['id'] = create_status['user']['id'] 
+            session['name'] = create_status['user']['name']
+            # we can redirect to the users profile page here
+            return redirect('/users')
+        else:
+            # set flashed error messages here from the error messages we returned from the Model
+            for message in create_status['errors']:
+                flash(message, 'regis_errors')
+            # redirect to the method that renders the form
+            return redirect('/')
+
+
+        # print ('^' * 25)
+        # print user
+        # print ('^' * 25)
+        # return redirect('/')
+
---
+++ app/models/Travel.py
@@ -0,0 +1,72 @@
+from system.core.model import Model
+from falsk import Flask, flash, session
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+NAME_REGEX = re.compile(r'^[a-zA-Z]*$')
+                        #is there upper case, number, at least 8 charater
+PW_REGEX = re.compile(r'^(?=.*?[A-Z])(?=.*?\d)[A-Za-z\d]{8,}$')
+
+class User(Model):
+    def __init__(self):
+        super(User, self).__init__()
+    """
+    Below is an example of a model method that queries the database for all users in a fictitious application
+    
+    Every model has access to the "self.db.query_db" method which allows you to interact with the database
+
+    def get_users(self):
+        query = "SELECT * from users"
+        return self.db.query_db(query)
+
+    def get_user(self):
+        query = "SELECT * from users where id = :id"
+        data = {'id': 1}
+        return self.db.get_one(query, data)
+
+    def add_message(self):
+        sql = "INSERT into messages (message, created_at, users_id) values(:message, NOW(), :users_id)"
+        data = {'message': 'awesome bro', 'users_id': 1}
+        self.db.query_db(sql, data)
+        return True
+    
+    def grab_messages(self):
+        query = "SELECT * from messages where users_id = :user_id"
+        data = {'user_id':1}
+        return self.db.query_db(query, data)
+
+    """
+
+     # We write our validations in model functions.
+    # They will look similar to those we wrote in Flask
+    EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
+    errors = []
+    # Some basic validation
+    if not info['f_name']:
+        errors.append('First name cannot be blank')
+    elif len(info['f_name']) < 2:
+        errors.append('First name must be at least 2 characters long')
+    if not info['l_name']:
+        errors.append('Last name cannot be blank')
+    elif len(info['l_name']) < 2:
+        errors.append('Last name must be at least 2 characters long')
+    if not info['email']:
+        errors.append('Email cannot be blank')
+    elif not EMAIL_REGEX.match(info['email']):
+        errors.append('Email format must be valid!')
+    if not info['passw']:
+        errors.append('Password cannot be blank')
+    elif len(info['passw']) < 8:
+        errors.append('Password must be at least 8 characters long')
+    elif info['passw'] != info['conf_passw']:
+        errors.append('Password and confirmation must match!')
+    # If we hit errors, return them, else return True.
+    if errors:
+        return {"status": False, "errors": errors}
+    else:
+        # Code to insert user goes here...
+        # Then retrieve the last inserted user.
+        get_user_query = "SELECT * FROM users ORDER BY id DESC LIMIT 1"
+        users = self.db.query_db(get_user_query)
+        return { "status": True, "user": users[0] }
+
